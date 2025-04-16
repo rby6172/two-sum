f7d6e5b6d32674a71e0d872bf869629856031e7d
boyeong <jessica6172@g.skku.edu>
2025-04-16 21:14:45 +0900

    Final submission

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from typing import List
 
 def twoSum(nums: List[int], target: int) -> List[int]:
-kdk
-return
-
+	dic={}
+	for i in range(len(nums)):
+		num=nums[i]
+		a=target-num
+		if a in dic:
+			return [dic[a], i]
+		dic[num]=i
 
